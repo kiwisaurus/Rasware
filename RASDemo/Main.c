abe3ed935d9f940076c029599680b4ef4641a1e6
Terrence Liu <terrence.liu@utexas.edu>
2015-11-03 17:11:28 -0600

    Update Main.c

--- RASDemo/Main.c
+++ RASDemo/Main.c
@@ -1,4 +1,4 @@
-#include "RASDemo.h"
+/*#include "RASDemo.h"
 
 #include <RASLib/inc/common.h>
 #include <RASLib/inc/gpio.h>
@@ -81,4 +81,31 @@ int main(void) {
                 break;
         }
     }
+}
+*/
+#include <RASLib/inc/common.h>
+#include <RASLib/inc/motor.h>
+#include <RASLib/inc/time.h>
+
+static tMotor *Motors[2];
+static tBoolean initialized = false;
+
+void InitMotors(void){
+	if(!initialized){
+		initialized = true;
+
+		Motors[0] = InitializeServoMotor(PIN_A5, false);
+		Motors[1] = InitializeServoMotor(PIN_A6, true);
+	}
+}
+
+void main(void){
+	InitMotors();
+	InitializeSystemTime();
+	SetMotor(Motors[0], 0.5f);
+	SetMotor(Motors[1], 0.5f);
+	while(GetTime()<10.0f){
+	}
+	SetMotor(Motors[0], 0.0f);
+	SetMotor(Motors[1], 0.0f);
 }
